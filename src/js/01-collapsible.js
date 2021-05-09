bb09550ac05ebcb91c2e14c5228d848f547bc50d
adrmenendez <adrmenendez@gmail.com>
2021-05-09 11:07:51 +0200

    add collapsible js

--- src/js/01-collapsible.js
+++ src/js/01-collapsible.js
@@ -1,21 +1,28 @@
 'use strict';
 
-const arrowIconDown = document.querySelector('.js-arrowdown');
-const arrowIconUp = document.querySelector('.js-arrowup');
-const collapsibleContent = document.querySelector('.js-collapsibleContainer');
+//design section
+const designSection = document.querySelector ('.js-design');
+const fillSection = document.querySelector ('.js-fill');
+const shareSection = document.querySelector ('.js-share');
 
-function showContent () {
-  arrowIconUp.classList.add('hidden');
-  arrowIconDown.classList.remove('hidden');
-  collapsibleContent.classList.remove('hidden');
+
+function showContent (event) {
+  const arrowUp = event.target.closest ('.js-arrowUp');
+  const arrowDown = event.target.closest ('.js-arrowDown');
+  const content = event.target.closest ('.js-collapsibleContainer');
+  if (arrowUp.classList ('.hidden')) {
+    arrowUp.classList.add ('.hidden');
+    arrowDown.classList.remove ('.hidden');
+    content.classList.remove ('.hidden');
+  } else {
+    arrowUp.classList.remove ('.hidden');
+    arrowDown.classList.add ('.hidden');
+    content.classList.add ('.hidden');
+  }
 }
 
-function hideContent () {
-  arrowIconUp.classList.remove('hidden');
-  arrowIconDown.classList.add('hidden');
-  collapsibleContent.classList.add('hidden');
-}
+designSection.addEventListener ('click', showContent);
+fillSection.addEventListener ('click', showContent);
+shareSection.addEventListener ('click', showContent);
 
-arrowIconUp.addEventListener ('click', showContent);
-arrowIconDown.addEventListener ('click', hideContent);
 
