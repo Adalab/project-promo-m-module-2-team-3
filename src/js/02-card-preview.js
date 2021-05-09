4a48198114dc560d34aacc761854e08b137c096c
cinthyajo <cinthya.jo@gmail.com>
2021-05-09 23:13:55 +0200

    Add comment job title card-preview.js

--- src/js/02-card-preview.js
+++ src/js/02-card-preview.js
@@ -9,3 +9,17 @@ function handleNameChange(event) {
 }
 
 inputValueName.addEventListener('change', handleNameChange);
+
+/* change en el input de job title:
+
+const jobInput = document.querySelector ('.js-inputjobtitle');
+const jobCard = document.querySelector ('.js-previewjob');
+
+function handlerInputChange (event) {
+  const inputValue =  jobInput.value;
+  const jobInput = event.currentTarget.Value;
+  jobCard.innerHTML = jobInput;
+
+}
+jobInput.addEventListener ("change", handlerInputChange);
+*/
