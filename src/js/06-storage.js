a35aa46fb00a83c438b39e3cc45beb5a2e6a7dec
adrmenendez <adrmenendez@gmail.com>
2021-05-20 16:51:33 +0200

    add storage js

---
+++ src/js/06-storage.js
@@ -0,0 +1,21 @@
+'use strict';
+const inputName = document.querySelector('.js-inputname');
+const inputJob = document.querySelector('.js-inputjobtitle');
+const inputEmail = document.querySelector('.js-inputEmail');
+const inputTel = document.querySelector('.js-inputTel');
+const inputIn = document.querySelector('.js-inputIn');
+const inputGit = document.querySelector('.js-inputGit');
+
+//recogemos los datos y los dejamos pintados por defecto
+function getData() {
+  if (localStorage.getItem('user')) {
+    let user = JSON.parse(localStorage.getItem('user'));
+    inputName.value = user.name;
+    inputJob.value = user.job;
+    inputEmail.value = user.email;
+    inputTel.value = user.phone;
+    inputIn.value = user.linkedin;
+    inputGit.value = user.github;
+  }
+}
+getData();
